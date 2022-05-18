453a0fa00a1c5fceede153a8a51486693aa6185f
shaggydev <jason@shaggydev.com>
2022-05-17 20:34:08 -0700

    Add datetime to os-class

--- projects/os-class/sample_scene.gd
+++ projects/os-class/sample_scene.gd
@@ -7,11 +7,19 @@ onready var width_spinbox = $display_container/window_size_container/width_spinb
 onready var x_spinbox = $display_container/window_position_container/x_spinbox
 onready var y_spinbox = $display_container/window_position_container/y_spinbox
 
-onready var clipboard_contents = $system_information/clipboard_contents
-onready var clipboard_source = $system_information/clipboard_source
+onready var clipboard_contents = $clipboard/clipboard_contents
+onready var clipboard_source = $clipboard/clipboard_source
+
+onready var datetime = $datetime
 
 func _ready() -> void:
 	monitor_size_label.text = 'Monitor size: %s\n(May be off on monitors with scaling)' % str(OS.get_screen_size())
+
+func _process(delta: float) -> void:
+	var current_datetime = OS.get_datetime()
+	var date = '%s-%s-%s' % [current_datetime['month'], current_datetime['day'], current_datetime['year']]
+	var time = '%s:%s:%s' % [current_datetime['hour'], current_datetime['minute'], current_datetime['second']]
+	datetime.text = '%s\n%s' % [date, time]
 
 func _on_fullscreen_toggled(button_pressed: bool) -> void:
 	OS.window_fullscreen = button_pressed
